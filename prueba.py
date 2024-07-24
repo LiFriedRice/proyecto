4ed0bb01a18552fb3240b706f6c058106b8888ff
Luis Fernando <friedricegit.com>
2024-07-24 10:20:27 -0400

    Inicio

---
+++ prueba.py
@@ -0,0 +1,9 @@
+# Definir dos números
+numero1 = 5
+numero2 = 3
+
+# Sumar los dos números
+resultado = numero1 + numero2
+
+# Mostrar el resultado
+print("La suma de", numero1, "y", numero2, "es:", resultado)
